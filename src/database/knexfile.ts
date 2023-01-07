42f63c775fd1dd838abfb4b65fbbe416637e5a39
Ose Ughu <oseughu@gmail.com>
2023-01-07 23:53:31 +0100

    - fix environment variables - add readme file - create test files

--- src/database/knexfile.ts
+++ src/database/knexfile.ts
@@ -9,11 +9,11 @@ const config: IKnexConfig = {
   development: {
     client: process.env.DB_CLIENT,
     connection: {
-      host: process.env.DB_HOST,
+      host: process.env.DB_HOST_DEV,
       port: +process.env.DB_PORT,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME
+      user: process.env.DB_USER_DEV,
+      password: process.env.DB_PASSWORD_DEV,
+      database: process.env.DB_NAME_DEV
     },
     pool: {
       min: 2,
@@ -27,11 +27,29 @@ const config: IKnexConfig = {
   production: {
     client: process.env.DB_CLIENT,
     connection: {
-      host: process.env.DB_HOST,
+      host: process.env.DB_HOST_PROD,
       port: +process.env.DB_PORT,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME
+      user: process.env.DB_USER_PROD,
+      password: process.env.DB_PASSWORD_PROD,
+      database: process.env.DB_NAME_PROD
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  test: {
+    client: process.env.DB_CLIENT,
+    connection: {
+      host: process.env.DB_HOST_TEST,
+      port: +process.env.DB_PORT,
+      user: process.env.DB_USER_TEST,
+      password: process.env.DB_PASSWORD_TEST,
+      database: process.env.DB_NAME_TEST
     },
     pool: {
       min: 2,
