0990368edcc785971b2df004b383f93aba738679
Ose Ughu <oseughu@gmail.com>
2023-01-12 07:24:56 +0100

    set config to depend on env vars

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,9 +1,18 @@
 import config from '#database/knexfile'
+import 'dotenv/config'
 import knex from 'knex'
+
+let db: any
 
 /* NOTE TO SELF: In prod, use dependency injection to create knex instance
 so db access can be mocked for tests */
 
-const db = knex(config.production)
+if (process.env.NODE_ENV === 'development') {
+  db = knex(config.development)
+} else if (process.env.NODE_ENV === 'production') {
+  db = knex(config.production)
+} else if (process.env.NODE_ENV === 'test') {
+  db = knex(config.test)
+}
 
 export default db
