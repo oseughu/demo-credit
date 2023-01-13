370dafdafbea3afb3f2f863dfa0c279c6b9d2362
Ose Ughu <oseughu@gmail.com>
2023-01-13 08:10:06 +0100

    - Finish writing tests

--- src/tests/auth.test.ts
+++ src/tests/auth.test.ts
@@ -1,9 +1,130 @@
+process.env.NODE_ENV = 'test'
 import db from '#database/db'
 import createServer from '#src/app'
-import payload from '#utils/payload'
-import 'dotenv/config'
+import { invalidLogin, invalidSignUp, validLogin, validSignUp } from '#utils/payloads'
+import { assert } from 'chai'
 import supertest from 'supertest'
 
-process.env.NODE_ENV = 'test'
+const app = createServer()
+let token: string
 
-// const app = createServer()
+describe('Authentication', async () => {
+  before((done) => {
+    db.seed.run({ directory: './src/database/seeds' }).then(() => {
+      done()
+    })
+  })
+
+  describe('POST /api/v1/register', () => {
+    describe('When all required fields are filled and passwords match', () => {
+      it('should create a user and return status code 201', (done) => {
+        supertest(app)
+          .post('/api/v1/register')
+          .send(validSignUp)
+          .set('Accept', 'application/json')
+          .expect('Content-Type', /json/)
+          .expect(201, done)
+      })
+    })
+
+    describe('When a required field is missing or passwords do not match', () => {
+      it('should return status code 400', (done) => {
+        supertest(app)
+          .post('/api/v1/register')
+          .send(invalidSignUp)
+          .set('Accept', 'application/json')
+          .expect(400, done)
+      })
+    })
+  })
+
+  describe('POST /api/v1/login', () => {
+    describe('When user exists and credentials are correct', () => {
+      it('should log a user in and return status code 200', (done) => {
+        supertest(app)
+          .post('/api/v1/login')
+          .send(validLogin)
+          .set('Accept', 'application/json')
+          .expect('Content-Type', /json/)
+          .expect(200)
+          .then((response) => {
+            assert(response.body.status, 'ok')
+            assert(response.body.message, 'logged in successfully')
+            done()
+          })
+      })
+    })
+
+    describe('When credentials are invalid or user does not exist', () => {
+      it('should return a 401 unauthorized error', (done) => {
+        supertest(app)
+          .post('/api/v1/login')
+          .send(invalidLogin)
+          .set('Accept', 'application/json')
+          .expect(401, done)
+      })
+    })
+  })
+
+  describe('GET /api/v1/user', () => {
+    describe('When a user is currently logged in', () => {
+      before((done) => {
+        supertest(app)
+          .post('/api/v1/login')
+          .send(validLogin)
+          .then((response) => {
+            token = response.body.token
+            done()
+          })
+      })
+
+      it('should return the user and return status code 200', (done) => {
+        supertest(app)
+          .get('/api/v1/user')
+          .set('Authorization', `Bearer ${token}`)
+          .set('Accept', 'application/json')
+          .expect('Content-Type', /json/)
+          .expect(200)
+          .then((response) => {
+            assert(response.body.email, validLogin.email)
+            done()
+          })
+      })
+    })
+
+    describe('When there is no user currently logged in', () => {
+      it('should return a 401 unauthorized error', (done) => {
+        supertest(app).get('/api/v1/user').expect(401, done)
+      })
+    })
+  })
+
+  describe('POST /api/v1/logout', () => {
+    describe('When a user is currently logged in and wants to log out', () => {
+      before((done) => {
+        supertest(app)
+          .post('/api/v1/login')
+          .send(validLogin)
+          .then((response) => {
+            token = response.body.token
+            done()
+          })
+      })
+
+      it('should clear the authorization header and return status code 200', (done) => {
+        supertest(app)
+          .post('/api/v1/logout')
+          .set('Authorization', `Bearer ${token}`)
+          .set('Accept', 'application/json')
+          .expect('Content-Type', /json/)
+          .expect(200, done)
+      })
+    })
+
+    describe('When there is no user currently logged in', () => {
+      it('should return a 401 unauthorized error', (done) => {
+        supertest(app).post('/api/v1/logout').expect(401, done)
+      })
+    })
+  })
+})
--- src/tests/transaction.test.ts
+++ src/tests/transaction.test.ts
@@ -1,9 +1,143 @@
+process.env.NODE_ENV = 'test'
 import db from '#database/db'
 import createServer from '#src/app'
-import payload from '#utils/payload'
-import 'dotenv/config'
+import {
+  invalidDeposit,
+  invalidTransfer400,
+  invalidTransfer404,
+  invalidWithdrawal,
+  validDeposit,
+  validLogin,
+  validTransfer,
+  validWithdrawal
+} from '#utils/payloads'
+import { assert } from 'chai'
 import supertest from 'supertest'
 
-process.env.NODE_ENV = 'test'
+const app = createServer()
+let token: string
+const user = db
+  .select('id', 'email', 'balance')
+  .from('users')
+  .where('email', '=', `${validLogin.email}`)
+  .first()
 
-// const app = createServer()
+describe('Transactions', () => {
+  beforeEach((done) => {
+    supertest(app)
+      .post('/api/v1/login')
+      .send(validLogin)
+      .then((response) => {
+        token = response.body.token
+        done()
+      })
+  })
+
+  describe('POST /api/v1/deposit', () => {
+    describe('When all required fields are filled and amount is valid', () => {
+      it('should process the deposit successfully and return status code 201', (done) => {
+        supertest(app)
+          .post('/api/v1/deposit')
+          .set('Authorization', `Bearer ${token}`)
+          .send(validDeposit)
+          .set('Accept', 'application/json')
+          .expect('Content-Type', /json/)
+          .expect(201)
+          .then((response) => {
+            assert(response.body.amount, (+validDeposit.amount).toLocaleString())
+            assert(response.body.message, 'deposit successful')
+            assert(response.body.balance, (+validDeposit.amount + +user.balance).toLocaleString())
+            done()
+          })
+      })
+    })
+
+    describe('When a required field is missing or an amount is invalid', () => {
+      it('should fail and return status code 400', (done) => {
+        supertest(app)
+          .post('/api/v1/deposit')
+          .set('Authorization', `Bearer ${token}`)
+          .send(invalidDeposit)
+          .expect(400, done)
+      })
+    })
+  })
+
+  describe('POST /api/v1/transfer', () => {
+    describe('When all required fields are filled with a valid amount and recipient', () => {
+      it('should process the transfer successfully and return status code 201', (done) => {
+        supertest(app)
+          .post('/api/v1/transfer')
+          .send(validTransfer)
+          .set('Authorization', `Bearer ${token}`)
+          .set('Accept', 'application/json')
+          .expect('Content-Type', /json/)
+          .expect(201)
+          .then((response) => {
+            assert(response.body.amount, (+validTransfer.amount).toLocaleString())
+            assert(response.body.message, 'transfer successful')
+            assert(response.body.recipient, `${validTransfer.recipient}`)
+            assert(response.body.balance, (+user.balance - +validTransfer.amount).toLocaleString())
+            done()
+          })
+      })
+    })
+
+    describe('When a required field is missing/amount is not valid/self-transfer/insufficient funds', () => {
+      it('should fail and return a 400', (done) => {
+        supertest(app)
+          .post('/api/v1/transfer')
+          .send(invalidTransfer400)
+          .set('Authorization', `Bearer ${token}`)
+          .set('Accept', 'application/json')
+          .expect(400, done)
+      })
+    })
+
+    describe('When sender/recipient does not exist', () => {
+      it('should fail and return a 404', (done) => {
+        supertest(app)
+          .post('/api/v1/transfer')
+          .send(invalidTransfer404)
+          .set('Authorization', `Bearer ${token}`)
+          .set('Accept', 'application/json')
+          .expect(404, done)
+      })
+    })
+  })
+
+  describe('POST /api/v1/withdrawal', () => {
+    describe('When all required fields are filled with a valid amount and account number', () => {
+      it('should process the withdrawal successfully and return status code 201', (done) => {
+        supertest(app)
+          .post('/api/v1/withdrawal')
+          .send(validWithdrawal)
+          .set('Authorization', `Bearer ${token}`)
+          .set('Accept', 'application/json')
+          .expect('Content-Type', /json/)
+          .expect(201)
+          .then((response) => {
+            assert(response.body.amount, (+validWithdrawal.amount).toLocaleString())
+            assert(response.body.message, 'withdrawal successful')
+            assert(response.body.recipient, `${validWithdrawal.recipient}`)
+            assert(
+              response.body.balance,
+              (+user.balance - +validWithdrawal.amount).toLocaleString()
+            )
+            done()
+          })
+      })
+    })
+
+    describe('When a required field is missing/amount or account number is not valid/insufficient funds', () => {
+      it('should fail and return a 400', (done) => {
+        supertest(app)
+          .post('/api/v1/withdrawal')
+          .send(invalidWithdrawal)
+          .set('Authorization', `Bearer ${token}`)
+          .set('Accept', 'application/json')
+          .expect(400, done)
+      })
+    })
+  })
+})
